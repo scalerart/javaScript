3d0383ca7e93c6c054075a2800b12516ba665157
scalerart <info.scalerart@gmail.com>
2025-09-02 17:46:12 +0600

    leapYearAdd

---
+++ 03. APPLY JAVASCRIPT CONCEPTS/1.convertInchToFeet.js
@@ -0,0 +1,12 @@
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var feet1 = inchToFeet(156);
+console.log(feet1);
+
+var feet2 = inchToFeet(288);
+console.log(feet2);
+
+var feet3 = inchToFeet(300);
+console.log(feet3);
